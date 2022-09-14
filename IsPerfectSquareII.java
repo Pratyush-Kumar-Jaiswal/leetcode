0223e16e6711f8e18274c0a8bbe7799249e73352
Pratyush Kumar Jaiswal <pratyush.jaiswal_cs20@gla.ac.in>
2022-09-14 23:11:28 +0530

    Create IsPerfectSquareII.java

---
+++ IsPerfectSquareII.java
@@ -0,0 +1,34 @@
+/*
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+
+Follow up: Do not use any built-in library function such as sqrt.
+
+ 
+
+Example 1:
+
+Input: num = 16
+Output: true
+Example 2:
+
+Input: num = 14
+Output: false
+ 
+
+Constraints:
+
+1 <= num <= 2^31 - 1
+*/
+
+
+class IsPerfectSquareII {
+    public boolean isPerfectSquare(int num) {
+        if(num==1||num==0){return true;}
+        for(int i=1;i*i>=0&&i*i<=num;i++){
+            if(i*i==num){
+                return true;
+            }
+        }
+        return false;
+    }
+}
