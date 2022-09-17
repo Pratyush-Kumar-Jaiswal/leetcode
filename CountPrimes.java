06d87f77031e13fc0b3ef1c658e757ed7af68331
Pratyush Kumar Jaiswal <pratyush.jaiswal_cs20@gla.ac.in>
2022-09-18 00:38:25 +0530

    Create CountPrimes.java

---
+++ CountPrimes.java
@@ -0,0 +1,45 @@
+/*
+Given an integer n, return the number of prime numbers that are strictly less than n.
+
+ 
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+Example 2:
+
+Input: n = 0
+Output: 0
+Example 3:
+
+Input: n = 1
+Output: 0
+*/
+
+
+
+class CountPrimes {
+    public int countPrimes(int n) {
+        if(n<=2){return 0;}
+        boolean []notprime=new boolean[n+1];
+        for(int i=0;i<notprime.length;i++){
+            notprime[i]=true;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(notprime[i]==true){
+                for(int j=i*i;j<=n;j+=i){
+                    notprime[j]=false;
+                }
+            }
+        }
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(notprime[i]==true){
+                count++;
+            }
+        }
+        return count;
+    }
+}
