b48f462f110ce86785eda9db98983e4e52165ce0
Pratyush Kumar Jaiswal <pratyush.jaiswal_cs20@gla.ac.in>
2022-11-13 01:11:13 +0530

    Create RepeatedSubstringPattern.java

---
+++ RepeatedSubstringPattern.java
@@ -0,0 +1,48 @@
+/*
+Given a string s, check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+
+ 
+
+Example 1:
+
+Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+Example 2:
+
+Input: s = "aba"
+Output: false
+Example 3:
+
+Input: s = "abcabcabcabc"
+Output: true
+Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of lowercase English letters.
+*/
+
+
+
+
+class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        int l=s.length();
+
+        for(int i=l/2;i>=1;i--){
+            if(l%i==0){
+                int num_repeats=l/i;
+                String subs=s.substring(0,i);
+                StringBuilder sbr=new StringBuilder();
+                for(int j=0;j<num_repeats;j++){
+                    sbr.append(subs);
+                }
+                if(sbr.toString().equals(s)){return true;}
+            }
+        }
+        return false;
+    }
+}
